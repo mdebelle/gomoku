c3bbb6f962e6dd2d557d43480971fb8cf508ad48
mdebelle <mdebelle@student.42.fr>
2016-03-27 21:25:20 +0200

    Add a stupid ia and one win rules

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
 	"github.com/veandco/go-sdl2/sdl"
 	"os"
 )
@@ -12,6 +13,7 @@ var winWidth, winHeight int = 800, 800
 
 func drawGrid(renderer *sdl.Renderer) {
 
+	_ = renderer.SetDrawColor(236, 0, 0, 0)
 	for i := 1; i < 20; i++ {
 		_ = renderer.DrawLine(40, 40 * i, 40 * 19, 40 * i)
 		_ = renderer.DrawLine(40 *i, 40, 40 * i, 40 * 19)
@@ -22,18 +24,52 @@ func drawGrid(renderer *sdl.Renderer) {
 
 func drawClic(renderer *sdl.Renderer, values [19][19]int) {
 
-
 	for i := 0; i < 19; i++ {
 		for j := 0; j < 19; j++ {
-			if values[i][j] == 1{
+			if values[j][i] == 1{
+				_ = renderer.SetDrawColor(0, 236, 0, 0)
+				for k := 0; k < 20; k++ {
+					_ = renderer.DrawLine(((i+1)*40)-10, ((j+1)*40)+(k-10), ((i+1)*40)+10, ((j+1)*40)+(k-10))
+				}
+			} else if values[j][i] == 2 {
+				_ = renderer.SetDrawColor(0, 0, 236, 0)
 				for k := 0; k < 20; k++ {
 					_ = renderer.DrawLine(((i+1)*40)-10, ((j+1)*40)+(k-10), ((i+1)*40)+10, ((j+1)*40)+(k-10))
 				}
 			}
 		}
-	}
+	}	
+}
 
+func mousePositionToGrid(val float64) int {
 	
+
+	fmt.Printf("What is val: %f\n", val)
+	t := int(math.Floor((val - 20) / 40))
+	fmt.Printf("What is t: %d\n", t)
+	if t < 0 {
+		t = 0
+	} else if t > 18{
+		t = 18
+	}
+	return t
+}
+
+
+func checkRules(values [19][19]int , nb int, y int, x int) ([19][19]int, int) {
+
+	success := true
+
+	for i:=0; i > -5 ; i-- {
+		if values[y+i][x+i] != nb {
+			success = false
+			break
+		}
+	}
+	if success == true {
+		return values, 1
+	}
+	return values, 0
 }
 
 
@@ -41,7 +77,10 @@ func run() int {
 
 	var event sdl.Event
 	var running bool
+	var stop bool
 	var err error
+
+	var play bool
 
 	values := [19][19]int{{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 							{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
@@ -80,7 +119,10 @@ func run() int {
 	defer renderer.Destroy()
 
 	running = true
+	stop = false
+	play = true
 	for running {
+
 		for event = sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch t := event.(type) {
 			case *sdl.QuitEvent:
@@ -89,22 +131,53 @@ func run() int {
 
 				fmt.Printf("[%d ms] MouseButton\ttype:%d\tid:%d\tx:%d\ty:%d\tbutton:%d\tstate:%d\n",
 					t.Timestamp, t.Type, t.Which, t.X, t.Y, t.Button, t.State)
-				y:=int(math.Floor(float64(t.Y) / 40) - 1)
-				x:=int(math.Floor(float64(t.X) / 40) - 1)
-				fmt.Printf("[%d][%d]\n", x, y)
-				values[y][x] = 1
-				fmt.Printf("[%d]\n", values[y][x])
+
+				if play == true && t.Type == 1025 && stop == false{
+					y:= mousePositionToGrid(float64(t.Y))
+					x:= mousePositionToGrid(float64(t.X))
+					
+					if values[y][x] == 0 {
+						var rules int
+						values[y][x] = 1
+						fmt.Printf("[%d]\n", values[y][x])
+						values, rules = checkRules(values, 1, y, x)
+						if rules == 1 {
+							fmt.Printf("C'est gagne pour le joueur stupide\n")
+							stop = true
+						}
+						play = false
+					}
+				} else {
+					fmt.Printf("Not your turn\n")
+				}
 
 			case *sdl.KeyUpEvent:
 				fmt.Printf("[%d ms] Keyboard\ttype:%d\tsym:%c\tmodifiers:%d\tstate:%d\trepeat:%d\n",
 					t.Timestamp, t.Type, t.Keysym.Sym, t.Keysym.Mod, t.State, t.Repeat)
 			}
 		}
+
+		if (play == false && stop == false) {
+	
+			x := rand.Int() % 19
+			y := rand.Int() % 19
+
+			if values[y][x] == 0 {
+				var rules int
+				values[y][x] = 2
+				fmt.Printf("[%d]\n", values[y][x])
+				values, rules = checkRules(values, 2, y, x)
+				if rules == 1 {
+					fmt.Printf("C'est gagne pour l'iA\n")
+					stop = true
+				}
+				play = true
+			}
+		}
+
 		_ = renderer.SetDrawColor(236, 240, 241, 0)
 		renderer.Clear()
-		_ = renderer.SetDrawColor(236, 0, 0, 0)
 		drawGrid(renderer)
-		_ = renderer.SetDrawColor(0, 236, 0, 0)
 		drawClic(renderer, values)
 		renderer.Present()
 	}
